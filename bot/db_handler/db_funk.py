384a5f7310bffb4bca9eeb6c118e69ff462e9585
cApitanYARE <deniswatch1@gmail.com>
2026-06-20 20:46:50 +0300

    update DB function to search vacancy

--- bot/db_handler/db_funk.py
+++ bot/db_handler/db_funk.py
@@ -41,7 +41,7 @@ try:
 except RuntimeError:
     pass
 
-async def create_table_vacancies(table_name='vacancie'):
+async def create_table_vacancies(table_name='vacancies'):
     async with db_manager as client:
         await client.create_table(
             table_name=table_name,
@@ -54,7 +54,6 @@ async def create_table_vacancies(table_name='vacancie'):
                 {'name': 'location', 'type': String(255)},
                 {'name': 'work_mode', 'type': String(255)},
                 {'name': 'salary', 'type': String(255)},
-                {'name': 'currency', 'type': String(10)},
                 {'name': 'experience', 'type': String(255)},
                 {'name': 'skills', 'type': String(255)},
                 {'name': 'nice_to_have', 'type': String(255)},
@@ -207,17 +206,17 @@ async def insert_data_user_get_messages(data: dict, table_name="messages_to_user
             logger.error(f"Error inserting message to user: {e}")
             return False
 
-async def insert_vacancies(data: dict, table_name='vacancie'):
+async def insert_vacancies(data: dict, table_name='vacancies'):
     skills_str = _parse_list_to_str(data.get('skills'))
     nice_to_have_str = _parse_list_to_str(data.get('nice_to_have'))
 
     query = text(f"""
         INSERT INTO {table_name} (
             is_active, created_at, company_name, job_position, location, 
-            work_mode, salary, currency, experience, skills, nice_to_have, more_about_it
+            work_mode, salary, experience, skills, nice_to_have, more_about_it
         )
         VALUES (TRUE, NOW(), :company_name, :job_position, :location, :work_mode, 
-                :salary, :currency, :experience, :skills, :nice_to_have, :more_about_it)
+                :salary, :experience, :skills, :nice_to_have, :more_about_it)
     """)
 
     async with db_manager.session() as session:
@@ -228,7 +227,6 @@ async def insert_vacancies(data: dict, table_name='vacancie'):
                 'location': data.get('location'),
                 'work_mode': data.get('work_mode'),
                 'salary': data.get('salary'),
-                'currency': data.get('currency'),
                 'experience': data.get('experience'),
                 'skills': skills_str,
                 'nice_to_have': nice_to_have_str,
@@ -245,26 +243,48 @@ async def select_user_data(user_id: int, table_name='users_reg'):
     async with db_manager as client:
         return await client.select_data(table_name=table_name, where_dict={'user_id': user_id}, one_dict=True)
     
-async def select_all_users(table_name='users_reg', count=False):
+async def select_all_users(table_name='users_reg'):
     async with db_manager as client:
         all_users = await client.select_data(table_name=table_name)
         return len(all_users) if count else all_users
 
-async def select_all_vacancies(table_name='vacancie'):
+async def select_all_vacancies(table_name='vacancies'):
     async with db_manager as client:
         return await client.select_data(table_name=table_name)
 
-async def select_search_vacancie(id_vacancy: int, table_name="vacancie"):
-    if isinstance(id_vacancy, int):
-        query = text(f"SELECT * FROM {table_name} WHERE id = :search_value")
-        params = {"search_value": int(id_vacancy)}
+async def select_search_vacancie(id_vacancy: int | str = None, embedding_vector: list = None, count=False, table_name="vacancies"):
+    if count:
+        query = text(f"SELECT COUNT(id) FROM {table_name}")
+        params = {}
     else:
-        query = text(f"SELECT * FROM {table_name} WHERE job_position ILIKE :search_value")
-        params = {"search_value": f"%{id_vacancy}%"}
+        if isinstance(id_vacancy, int):
+            query = text(f"SELECT * FROM {table_name} WHERE id = :search_value")
+            params = {"search_value": id_vacancy}
+
+        elif embedding_vector is not None:
+            query = text(f"""
+                SELECT *, (embedding <=> :vector) as distance 
+                FROM {table_name} 
+                WHERE status = 'active' 
+                ORDER BY embedding <=> :vector ASC 
+                LIMIT :limit_val
+            """)
+            params = {
+                "vector": embedding_vector,
+                "limit_val": 3               
+            }
+
+        else:
+            query = text(f"SELECT * FROM {table_name} WHERE job_position ILIKE :search_value")
+            params = {"search_value": f"%{id_vacancy}%"}
 
     async with db_manager.session() as session:
         result = await session.execute(query, params)
+        if count:
+            return result.scalar()
+            
         return [dict(v) for v in result.mappings().all()]
+
 
 async def select_data_for_hr_about_review(id_vacancie, id_user, table_name="for_hr_about_review"):
     query = text(f"""
@@ -300,7 +320,7 @@ async def select_data_all_reviews(table_name="for_hr_about_review"):
         LEFT JOIN vacancie v ON r.id_vacancie = v.id
         LEFT JOIN users_reg u ON r.id_user = u.user_id
         WHERE r.id_vacancie IS NOT NULL
-          AND r.status = 'pending'
+          AND r.status = 'reject'
     """)
     async with db_manager.session() as session:
         result = await session.execute(query)
@@ -338,15 +358,15 @@ async def select_data_user_get_messages(user_id: int, table_name="messages_to_us
             return False
 
 #update table 
-async def update_user_status_as_false(user_id, vacancy_id, table_name="reviews"):
+async def update_user_status(user_id, vacancy_id, status, table_name="for_hr_about_review"):
     query = text(f"""
         UPDATE {table_name}
-        SET status = FALSE
+        SET status = :status
         WHERE id_user = :id_user AND id_vacancie = :id_vacancie
     """)
     async with db_manager.session() as session:
         try:
-            await session.execute(query, {'id_user': int(user_id), 'id_vacancie': int(vacancy_id)})
+            await session.execute(query, {'id_user': int(user_id), 'id_vacancie': int(vacancy_id), 'status': str(status)})
             await session.commit()
         except Exception as e:
             logger.error(f"Error updating status: {e}")
